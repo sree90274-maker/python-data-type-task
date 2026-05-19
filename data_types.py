8018bb197ad341eb51f2a42f7655c55a425396d4
sree90274-maker <sree90274@gmail.com>
2026-05-19 12:38:50 +0530

    conversions

---
+++ data_types.py
@@ -0,0 +1,26 @@
+num = 10
+print("decimal:", num)
+print("binary:", bin(num))
+print("0ctal:", oct(num))
+
+fruits = ["apple", "apple", "mango"]
+print(fruits)
+
+colors = ("red", "green", "black")
+print(colors)
+
+for i in range(5):
+    print(i)
+
+
+std={23781235:"durga",23781632:"nani"}
+print(std)
+print(type(std))
+
+a=10
+b=20
+print(a>b)
+print(a<b)
+
+numbers = (1,2,3,4,5)
+print(numbers)
